f54edeb10d383e9bf50f151b940a5e533cca4623
Olorode Adebanke <108427424+Jenrade112@users.noreply.github.com>
2023-02-10 15:06:40 +0100

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,17 @@
+let firstName='Banke';
+let lastName='olorode';
+let stuterntrack='frontend developer';
+let skills='java,css,html';
+let favoritecolor='Black';
+let yearofindependence='1960';
+
+
+console.log(firstName);
+console.log(lastName);
+console.log(stuterntrack);
+console.log(skills);
+console.log(favoritecolor);
+console.log(yearofindependence);
+
+
+console.log("My name is "+ firstName + ". And my last name is " +lastName + ". My sturtern track is " + stuterntrack + ".My skills are " +skills + ". My favorite color is " +  favoritecolor + ". Nigeria got their independence in the year " + yearofindependence +"."  )
